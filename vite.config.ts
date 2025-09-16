e6b2da51455c980e040812eb30c8d8be502a280a
RomanKorotun <127940013+RomanKorotun@users.noreply.github.com>
2025-09-16 13:10:12 +0300

    init project

---
+++ vite.config.ts
@@ -0,0 +1,39 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import svgr from "vite-plugin-svgr";
+import viteCompression from "vite-plugin-compression";
+import Inspect from "vite-plugin-inspect";
+import { visualizer } from "rollup-plugin-visualizer";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    svgr(),
+    viteCompression({
+      algorithm: "brotliCompress",
+      ext: ".br",
+    }),
+    Inspect(),
+  ],
+  build: {
+    minify: "terser",
+    terserOptions: {
+      format: { comments: false },
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+    rollupOptions: {
+      plugins: [
+        visualizer({
+          filename: "stats.html",
+          open: true,
+        }),
+      ],
+    },
+  },
+});
